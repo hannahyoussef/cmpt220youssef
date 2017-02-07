95c308c88324991e87ca2f4043c3b56c9ca8b654
hannahyoussef <hannah.youssef1@marist.edu>
2017-02-06 21:35:10 -0500

    Problem6_1.java
    Problem 6.1 for Lab 3

---
+++ labs/3/Problem6_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem6_1 {
+
+	public static void main(String[] args) {
+
+        for (int i = 1; i < 101; i++){
+            System.out.printf("%7d ", getPentagonalNumber(i));
+            if (i % 10 == 0)
+                System.out.println();
+        }
+    }
+
+    public static int getPentagonalNumber(int n){
+        return (n * (3 * n - 1)) / 2;
+    }
+}
