55246f14f88074e8f87a896d2c8230bed75ee0b5
hannahyoussef <hannah.youssef1@marist.edu>
2017-03-04 18:08:59 -0500

    Location.java
    Location Class for Problem 9.13 for Lab 6

---
+++ labs/6/Problem9_13/Location.java
@@ -0,0 +1,24 @@
+// Implement Location class
+public class Location {
+
+	// Data foelds
+	int row; 			// Row index of maximal value
+	int column; 		// Column index of maximal value
+	double maxValue; 	// Maximal value
+	
+	// Constructs a default Location object
+	Location(double[][] a) {
+		maxValue = a[0][0];
+		row = 0;
+		column = 0;
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				if (a[i][j] > maxValue) {
+					maxValue = a[i][j];
+					row = i;
+					column = j;
+				}
+			}
+		}
+	}
+}
