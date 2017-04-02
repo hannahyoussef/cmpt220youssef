60267ea797426545da0a751697f64a9c96faa02e
Juan Arias <juan.arias@marist.edu>
2017-04-01 20:02:41 -0400

    Feedback on Lab 8

--- labs/8/Problem 11_13/Problem11_13.java
+++ labs/8/Problem 11_13/Problem11_13.java
@@ -28,6 +28,8 @@ public class Problem11_13 {
 	}
 
 	/** Removes the duplicate elements from an array list of integers */
+	// JA: A better approach was to replicate the arraylist without the duplicates
+	// JA: The code below does not remove three consecutive duplicates
 	public static void removeDuplicate(ArrayList<Integer> list) {
 		for (int i = 0; i < list.size() - 1; i++) {
 			for (int j = i + 1; j < list.size(); j++) {
