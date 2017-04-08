b4b36b5c9e0fe6ce955bedd57b470d68c3810d28
Juan Arias <juan.arias@marist.edu>
2017-04-08 16:18:49 -0400

    Feedback on Lab 9

--- labs/9/Problem12_7/Problem12_7.java
+++ labs/9/Problem12_7/Problem12_7.java
@@ -1,4 +1,11 @@
 public class Problem12_7 {
+
+	// JA: Need a main methog to test this
+	public static void main(String[] args) {
+		System.out.println(bin2Dec("111"));
+		System.out.println(bin2Dec("345"));
+	}
+
 	public static int bin2Dec(String binaryString) throws NumberFormatException {
 		int decimal = 0;
 		for (int i = 0, j = binaryString.length() - 1; 
